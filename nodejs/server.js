3647e17f16ab1ae9f7c38cdbf509bf72e3644563
diegosoek <diegojosesoek@hotmail.com>
2017-07-18 17:41:51 -0300

    Connection

--- nodejs/server.js
+++ nodejs/server.js
@@ -10,8 +10,11 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(8890, function(){
-  console.log('listening on *:3000');
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 /*
