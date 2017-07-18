bb48329588f037ce19ccd426d8ed2e9c8207cf13
diegosoek <diegojosesoek@hotmail.com>
2017-07-18 17:42:46 -0300

    Connection 2

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,10 +8,6 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
